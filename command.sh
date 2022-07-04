0437dfd1e175ed1815e81229c84ada2bec742da3
heudersena <documento_arquivo@hotmail.com>
2022-07-04 11:37:07 -0300

    get command branch main

--- command.sh
+++ command.sh
@@ -1,7 +1,11 @@
 # Docs => https://blog.betrybe.com/git/git-branch/
+# Docs => https://www.varonis.com/pt-br/blog/git-branching
 
 # Entrar em uma brach existente
 exec git checkout nome-da-brach
+
+# Criação de um branch a partir de outro branch
+exec git checkout -b branch1 branch2
 
 # criar uma nova branch => copy
 exec git checkout -b copy
@@ -12,3 +16,7 @@ exec git push --set-upstream origin copy
 # Listar todas as branch
 exec git branch -a     # remotas
 exec git branch --list # local
+
+# Merge de branches em um repositório local
+exec git checkout main
+exec git merge copy

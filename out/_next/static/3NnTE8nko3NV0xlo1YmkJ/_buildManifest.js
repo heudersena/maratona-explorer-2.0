83df3edadc0cb887687a5930be28c857280e9b9f
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2022-07-04 13:20:15 -0300

    Delete out directory

--- out/_next/static/3NnTE8nko3NV0xlo1YmkJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-271cb7e7cd7e1bd4.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/register":["static/chunks/pages/register-90b3fa9be7db4a59.js"],sortedPages:["/","/_app","/_error","/register"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

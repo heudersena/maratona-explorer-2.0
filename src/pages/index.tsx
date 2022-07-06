d915d125a31acaa133053b4d410418eae6a8b587
heudersena <documento_arquivo@hotmail.com>
2022-07-06 08:37:02 -0300

    CONFIG: pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
-import { Box, Flex, Heading,Divider } from "@chakra-ui/react";
+import { Box, Flex, Heading, Divider } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => (
-  <Flex
-    bg="me.55"
-    h="100vh"
-    w="100vw"
-    alignItems="start"
-    justifyContent="center"
-    m="auto"
-    color="me.60"
-  >
-    <Heading>dsad</Heading>
-    <Box>
-    <Divider variant="dashed" h="50px" />
-    </Box>
-
-  </Flex>
+    <Flex
+        bg="me.55"
+        h="100vh"
+        w="100vw"
+        alignItems="start"
+        justifyContent="center"
+        m="auto"
+        color="me.60"
+    >
+        <Heading>dsad</Heading>
+        <Box>
+            <Divider variant="dashed" h="50px" />
+        </Box>
+    </Flex>
 );
 
 export default Home;
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Register: NextPage = () => {
-  return <h1>register</h1>;
+    return <h1>register</h1>;
 };
 
 export default Register;
